817a788bce0838f925f5e8dc275a0bd3c697e45e
Dominik Mrázek <mrazekdominik@centrum.cz>
2023-05-15 08:13:43 +0200

    Basic functionality completed, Managed to connect the app to a SQLite database.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,8 +19,52 @@ namespace WPFTo_Do {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        List<TaskModel> tasksList = new List<TaskModel>();
         public MainWindow() {
             InitializeComponent();
+            tasksList = SqliteDataAccess.LoadTasks();       //nacteni databaze s Tasky
+            foreach (TaskModel task in tasksList) {
+                ListBoxAllTasks.Items.Add(task);
+            }
+
+
+
         }
+     
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            TaskModel newTask = new TaskModel(TextBoxTask.Text, DaysComboBox.SelectionBoxItem.ToString(), TextBoxNotes.Text);   //vytvoreni instance Task
+            ListBoxAllTasks.Items.Add(newTask);  //pridavam Task jako TaskModel object
+            SqliteDataAccess.saveTask(newTask);     //pridani do databaze
+            //tasks.Add(newTask);
+        }
+
+        private void RemoveTaskBtn_Click(object sender, RoutedEventArgs e) {
+            TaskModel selectedTask = (TaskModel)ListBoxAllTasks.SelectedItem;
+            if (selectedTask != null) {     //hlidam si, ze jsem zvolil nejaky Task
+                ListBoxAllTasks.Items.Remove(selectedTask);
+                SqliteDataAccess.deleteTask(selectedTask);
+            }
+        }
+
+        private void RemoveAllButton_Click(object sender, RoutedEventArgs e) {
+            ListBoxAllTasks.Items.Clear();
+            SqliteDataAccess.deleteAllTasks();
+        }
+
+        private void MainWindow1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            this.DragMove();            //nepouzivam default window style, takze okno nebylo dragable, musel jsem vyuzit event handler
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+
+        //private void LoadTasksButton_Click(object sender, RoutedEventArgs e) {
+        //    //tasksList = SqliteDataAccess.LoadTasks();
+        //    //foreach (TaskModel task in tasksList) {
+        //    //    ListBoxAllTasks.Items.Add(task);
+        //    //}
+        //}
     }
 }
